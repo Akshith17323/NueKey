dca5ab3ffe7237642c973e29f2f31938bac3e2d8
Akshith17323 <akhi171356@gmail.com>
2025-07-21 10:50:17 +0530

    Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -32,22 +32,26 @@ function Navbar() {
     //     </div>
     //   </div>
     // </div>
-    <nav className='fixed z-50 flex justify-center mt-2.5 p-2 w-[90vw] bg-stone-950 text-white'>
-        <div className=''>NueKey</div>
-        <a>Home</a>
-        <a className="link-item nav-animate">Contact Us</a>
-        <a className="link-item nav-animate">Wishlist</a>
-        <a className='link-item nav-animate'>About Us</a>
-    <div className='flex gap-1'>
-        <button className="nav-animate bg-stone-950 rounded-lg h-8 px-3 border border-white hover:bg-white hover:text-stone-950 transition">
-          Login
-        </button>
-        <button className="nav-animate bg-stone-950 rounded-lg h-8 px-3 border border-white hover:bg-white hover:text-stone-950 transition">
-          SignUp
-        </button>
-    </div>
+    <div className='flex justify-center '>
+        <nav className='fixed z-50 flex justify-between  mt-2.5 py-1 px-4 w-[90vw] bg-stone-950 text-white'>
+            <div className='logo text-2xl'>NueKey</div>
+            <div className='flex gap-4 items-center'>
+                <a>Home</a>
+                <a className="link-item nav-animate">Contact Us</a>
+                <a className="link-item nav-animate">Wishlist</a>
+                <a className='link-item nav-animate'>About Us</a>
+            </div>
+            <div className='flex gap-1'>
+                <button className="nav-animate bg-stone-950 rounded-lg h-8 px-3 border border-white hover:bg-white hover:text-stone-950 transition">
+                Login
+                </button>
+                <button className="nav-animate bg-stone-950 rounded-lg h-8 px-3 border border-white hover:bg-white hover:text-stone-950 transition">
+                SignUp
+                </button>
+            </div>
 
-    </nav>
+        </nav>
+    </div>
   );
 }
 
